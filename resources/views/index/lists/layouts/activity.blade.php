f576c3aa23c0e145000296f91dcab880dc0d1c2e
ganlvtech <ganlvtech@qq.com>
2017-04-30 21:06:25 +0800

    删除无用文件

--- resources/views/index/lists/layouts/activity.blade.php
+++
@@ -1,9 +0,0 @@
-@for($i = 0; $i < count($activities); $i += 3)
-    <div class="list clearfix">
-        @for($j = 0; $j < 3, $i + $j < count($activities); ++$j)
-            @if($activity)
-                @include('index.lists.layouts.activity_item')
-            @endif
-        @endfor
-    </div>
-@endfor
